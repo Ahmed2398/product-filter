9772c48ed9834c570e54bc0f96db764d1cd1ac5b
Ahmed Ashraf <ahmedashraf2398@gmail.com>
2023-03-30 17:41:27 +0200

    first commit

---
+++ src/products-data.js
@@ -0,0 +1,99 @@
+import img1 from '../src/assets/images/1.png';
+import img2 from '../src/assets/images/2.png';
+import img3 from '../src/assets/images/3.png';
+import img4 from '../src/assets/images/4.png';
+import img5 from '../src/assets/images/5.png';
+import img6 from '../src/assets/images/6.png';
+import img7 from '../src/assets/images/7.png';
+import img8 from '../src/assets/images/8.png';
+import img9 from '../src/assets/images/9.png';
+import img10 from '../src/assets/images/10.png';
+import img11 from '../src/assets/images/11.png';
+import img12 from '../src/assets/images/12.png';
+import img13 from '../src/assets/images/13.png';
+export const products = [
+  {
+    id: 1,
+    title: "Gown Product",
+    category: "gown",
+    price: 15.99,
+    img: img13,
+  },
+  {
+    id: 2,
+    title: "Shirt Product",
+    category: "shirt",
+    price: 15.99,
+    img: img8,
+  },
+  {
+    id: 3,
+    title: "Shirt Product",
+    category: "shirt",
+    price: 15.99,
+    img: img3,
+  },
+  {
+    id: 4,
+    title: "Jewelry Product",
+    category: "jewelry",
+    price: 25.99,
+    img: img9,
+  },
+  {
+    id: 5,
+    title: "Jewelry Product",
+    category: "jewelry",
+    price: 15.99,
+    img: img9,
+  },
+  {
+    id: 6,
+    title: "Jewelry Product",
+    category: "jewelry",
+    price: 15.99,
+    img: img10,
+  },
+  {
+    id: 7,
+    title: "Watch Product",
+    category: "watch",
+    price: 15.99,
+    img: img1,
+  },
+  {
+    id: 8,
+    title: "Watch Product",
+    category: "watch",
+    price: 15.99,
+    img: img2,
+  },
+  {
+    id: 9,
+    title: "Shirt Product",
+    category: "shirt",
+    price: 35.99,
+    img: img4,
+  },
+  {
+    id: 10,
+    title: "Watch Product",
+    category: "watch",
+    price: 35.99,
+    img: img2,
+  },
+  {
+    id: 11,
+    title: "Shirt Product",
+    category: "shirt",
+    price: 35.99,
+    img: img7,
+  },
+  {
+    id: 12,
+    title: "Shirt Product",
+    category: "shirt",
+    price: 35.99,
+    img: img10,
+  },
+];
